bc0623aa90705ed02efbbd9590ed6d461518432e
Aadi Poddar <aadipoddarmail@gmail.com>
2023-11-15 22:56:00 +0530

    Add expression evaluator
    This adds the basics we need, specifically a lexer, a parser, and
    diagnostics that will lay the foundation for our compiler.

---
+++ mc/CodeAnalysis/SyntaxToken.cs
@@ -0,0 +1,25 @@
+﻿namespace Minsk.CodeAnalysis
+{
+    // Represents a word in our languge
+    class SyntaxToken : SyntaxNode
+    {
+        public SyntaxToken(SyntaxKind kind, int position, string text, object value)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+            Value = value;
+        }
+
+        public override SyntaxKind Kind { get; }
+
+        public int Position { get; }
+        public string Text { get; }
+        public object Value { get; }
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            return Enumerable.Empty<SyntaxNode>();
+        }
+    }
+}
