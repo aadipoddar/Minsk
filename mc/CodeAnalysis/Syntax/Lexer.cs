267154fb54067639f61dd136beff41d895434da9
Aadi Poddar <aadipoddarmail@gmail.com>
2023-11-16 21:38:28 +0530

    Add support for == and !=
    Also fixes an error in the binder that doesn't correctly compute the
    resulting expression. It should have used the operator's type but still
    uses the hardcoded operand/left resulting types.

--- mc/CodeAnalysis/Syntax/Lexer.cs
+++ mc/CodeAnalysis/Syntax/Lexer.cs
@@ -94,8 +94,6 @@
                     return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
                 case ')':
                     return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
-                case '!':
-                    return new SyntaxToken(SyntaxKind.BangToken, _position++, "!", null);
                 case '&':
                     if (Lookahead == '&')
                         return new SyntaxToken(SyntaxKind.AmpersandAmpersandToken, _position += 2, "&&", null);
@@ -104,6 +102,15 @@
                     if (Lookahead == '|')
                         return new SyntaxToken(SyntaxKind.PipePipeToken, _position += 2, "||", null);
                     break;
+                case '=':
+                    if (Lookahead == '=')
+                        return new SyntaxToken(SyntaxKind.EqualsEqualsToken, _position += 2, "==", null);
+                    break;
+                case '!':
+                    if (Lookahead == '=')
+                        return new SyntaxToken(SyntaxKind.BangEqualsToken, _position += 2, "!=", null);
+                    else
+                        return new SyntaxToken(SyntaxKind.BangToken, _position++, "!", null);
             }
 
             _diagnostics.Add($"ERROR: Bad Character Input: '{Current}");
