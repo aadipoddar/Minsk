a93ff0dcd4410ef99045d3e4b285641010f1e903
Aadi Poddar <aadipoddarmail@gmail.com>
2024-01-04 17:27:37 +0530

    Replace binding logic with proper symbols

--- Minsk/CodeAnalysis/Binding/BoundVariableExpression.cs
+++ Minsk/CodeAnalysis/Binding/BoundVariableExpression.cs
@@ -2,14 +2,13 @@ namespace Minsk.CodeAnalysis.Binding
 {
     internal sealed class BoundVariableExpression : BoundExpression
     {
-        public BoundVariableExpression(string name, Type type)
+        public BoundVariableExpression(VariableSymbol variable)
         {
-            Name = name;
-            Type = type;
+            Variable = variable;
         }
 
         public override BoundNodeKind Kind => BoundNodeKind.VariableExpression;
-        public string Name { get; }
-        public override Type Type { get; }
+        public VariableSymbol Variable { get; }
+        public override Type Type => Variable.Type;
     }
 }
