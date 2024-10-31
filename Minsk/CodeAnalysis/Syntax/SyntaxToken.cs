85b92183659ed8c8e489c9272c378f36dc8d7b96
Aadi Poddar <aadipoddarmail@gmail.com>
2024-11-01 03:12:33 +0530

    Fix null reference for cases where the token was inserted

--- Minsk/CodeAnalysis/Syntax/SyntaxToken.cs
+++ Minsk/CodeAnalysis/Syntax/SyntaxToken.cs
@@ -16,5 +16,5 @@ public sealed class SyntaxToken : SyntaxNode
 	public int Position { get; }
 	public string Text { get; }
 	public object Value { get; }
-	public override TextSpan Span => new TextSpan(Position, Text.Length);
+	public override TextSpan Span => new TextSpan(Position, Text?.Length ?? 0);
 }
