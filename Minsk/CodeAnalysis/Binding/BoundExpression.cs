cb089e955012a8f2a5fcd1a67f93e163059b017a
Aadi Poddar <aadipoddarmail@gmail.com>
2024-11-03 00:35:19 +0530

    Clean Up
    Move to src folder

--- Minsk/CodeAnalysis/Binding/BoundExpression.cs
+++
@@ -1,6 +0,0 @@
-namespace Minsk.CodeAnalysis.Binding;
-
-internal abstract class BoundExpression : BoundNode
-{
-	public abstract Type Type { get; }
-}
---
+++ src/Minsk/CodeAnalysis/Compilation.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Minsk.CodeAnalysis.Binding;
+using Minsk.CodeAnalysis.Syntax;
+
+namespace Minsk.CodeAnalysis;
+
+public sealed class Compilation
+{
+	public Compilation(SyntaxTree syntaxTree)
+	{
+		SyntaxTree = syntaxTree;
+	}
+
+	public SyntaxTree SyntaxTree { get; }
+
+	public EvaluationResult Evaluate(Dictionary<VariableSymbol, object> variables)
+	{
+		var binder = new Binder(variables);
+		var boundExpression = binder.BindExpression(SyntaxTree.Root);
+
+		var diagnostics = SyntaxTree.Diagnostics.Concat(binder.Diagnostics).ToImmutableArray();
+		if (diagnostics.Any())
+			return new EvaluationResult(diagnostics, null);
+
+		var evaluator = new Evaluator(boundExpression, variables);
+		var value = evaluator.Evaluate();
+		return new EvaluationResult(ImmutableArray<Diagnostic>.Empty, value);
+	}
+}
